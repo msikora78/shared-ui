5535c355f9b2215e8b1adf0dd0ed68738ab66e7c
Francis Fortier <ffortier@ffortier-ubuntu.(none)>
2013-02-06 12:38:03 -0500

    bash to build tm-all.min.js

---
+++ build/concat.sh
@@ -0,0 +1,9 @@
+cat ../js/tm.js \
+    ../js/tm.cookie.js \
+    ../js/tm.string.js \
+    ../js/tm.date.js \
+    ../js/tm/widgets/widgetFactory.js \
+    ../js/tm/widgets/modalDialog.js \
+    > tm-all.js
+
+uglifyjs -o tm-all.min.js tm-all.js
