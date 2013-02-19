207a9210ac80c2abf06a74398f67ca4f7b1fa009
ffortier <francis.fortier@gmail.com>
2013-02-19 09:03:17 -0500

    add a widgetBuilder to allow optimisation

--- plugin/widget.js
+++ plugin/widget.js
@@ -10,15 +10,7 @@ define(['tm/widgets/widgetFactory'], function(widgetFactory) {
 				load(widgetFactory.make(widgetName, value));
 			});
 		},
-		write: function(pluginName, moduleName, write) {
-			var widgetName = moduleName.replace(/^.*?(\w)(\w*)$/, function(a, b, c) {
-				return 'tm' + b.toUpperCase() + c;
-			});
-
-			write("define('" + pluginName + "!" + moduleName + "', ['tm/widgets/widgetFactory', '" + moduleName + "'], function(widgetFactory, proto) { " +
-				"return widgetFactory.make('" + widgetName + "', proto); " +
-				"});\n");
-		}
-	}
+		pluginBuilder: 'widgetBuilder'
+	};
 
 });
