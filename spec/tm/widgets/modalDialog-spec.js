15621d1150ab486f3fa6cd02bd62ff216e580b42
Jean-Francois Renaud <jeanfrancois.renaud@gmail.com>
2013-03-14 11:55:54 -0400

    ModalDialog Part IV

--- spec/tm/widgets/modalDialog-spec.js
+++ spec/tm/widgets/modalDialog-spec.js
@@ -15,35 +15,54 @@ if (isRequire) {
             for (var version in jquery) {
                 if (jquery.hasOwnProperty(version)) {
                     var ModalDialog = modalDialogInjectable(jquery[version], gadgets);
-                    runTest(jquery[version], Util, ModalDialog);
+                    runTest(jquery[version], Util, false, ModalDialog);
+                    runTest(jquery[version], Util, true, ModalDialog);
                 }
             }
         });
     });
 } else {
     describe('tm.widgets.modalDialog', function() {
-        runTest($, tm.widgets.util);
+        runTest($, tm.widgets.util, false);
+        runTest($, tm.widgets.util, true);
     });
 }
 
-function runTest($, Util, ModalDialog) {
-    describe('with jquery v' + $.fn.jquery, function() {
+function runTest($, Util, isLarge, ModalDialog) {
+    describe(isLarge ? 'large with jquery v' + $.fn.jquery : 'with jquery v' + $.fn.jquery, function() {
         var $container, $modal, widget;
 
         function createModal() {
-            $modal = $('\
-                <div>\
-                    <div class="modal-header">\
-                        <h3>Title Text</h3>\
-                    </div>\
-                    <div class="modal-body">\
-                        <p>Lorem ipsum dolor sit amet</p>\
-                    </div>\
-                    <div class="modal-footer">\
-                        <button type="button" class="btn">Text</button>\
-                        <button type="button" class="btn btn-primary">Text</button>\
-                    </div>\
-                </div>');
+            if (isLarge) {
+                $modal = $('\
+                    <div class="large">\
+                        <div class="modal-header">\
+                            <h3>Title Text</h3>\
+                        </div>\
+                        <div class="modal-body">\
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>\
+                        </div>\
+                        <div class="modal-footer">\
+                            <button type="button" class="btn">Text</button>\
+                            <button type="button" class="btn btn-primary">Text</button>\
+                        </div>\
+                    </div>');
+
+            } else {
+                $modal = $('\
+                    <div>\
+                        <div class="modal-header">\
+                            <h3>Title Text</h3>\
+                        </div>\
+                        <div class="modal-body">\
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>\
+                        </div>\
+                        <div class="modal-footer">\
+                            <button type="button" class="btn">Text</button>\
+                            <button type="button" class="btn btn-primary">Text</button>\
+                        </div>\
+                    </div>');
+            }
             $container = $('<div class="tm360" />').append($modal);
             $('body').append($container);
         }
@@ -84,9 +103,21 @@ function runTest($, Util, ModalDialog) {
         it('should have a #FFF background with a 10px border that’s #000 at 25% opacity', function() {
             expect($modal.css('background-color')).toBe(Util.convertHexaToRgb('ffffff'));
             Util.evaluateBorderWidth($modal, '10px');
-            //Util.evaluateBorderColor($modal, Util.convertHexaToRgba("000000", '0.25'));
+            //Util.evaluateBorderColor($modal, Util.parseShadowValue(Util.convertHexaToRgba("000000", '0.25')));
         });
-        
+
+        if (isLarge) {
+            it('should be 636px wide (inner) and 656px wide (outer)', function() {
+                expect($modal.innerWidth()).toBe(636);
+                expect($modal.outerWidth()).toBe(656);
+            });
+        } else {
+            it('should be 482px wide (inner) and 502px wide (outer)', function() {
+                expect($modal.innerWidth()).toBe(482);
+                expect($modal.outerWidth()).toBe(502);
+            });
+        }
+
         describe('Title section', function() {
             it('should be 18px Museo Sans 500 #4F5158', function() {
                 var $title = $modal.find('.modal-header h3');
@@ -98,15 +129,15 @@ function runTest($, Util, ModalDialog) {
 
             it('should have margin of 20px 20px 30px 20px', function() {
                 var $modalHeader = $modal.find('.modal-header');
-                
-                //expect(Util.calculateDistance($modalHeader, 'top')).toBe('20px'); // currently set to 16px...
+
+                //expect(Util.calculateDistance($modalHeader, 'top')).toBe('20px');
                 expect(Util.calculateDistance($modalHeader, 'right')).toBe('20px');
-                //expect(Util.calculateDistance($modalHeader, 'bottom')).toBe('30px'); // seems strage
+                //expect(Util.calculateDistance($modalHeader, 'bottom')).toBe('30px');
                 expect(Util.calculateDistance($modalHeader, 'left')).toBe('20px');
             });
         });
 
-        describe('Body section', function() { 
+        describe('Body section', function() {
             it('should be 14px Arial Regular #4F5158', function() {
                 var $body = $modal.find('.modal-body p');
 
@@ -117,7 +148,7 @@ function runTest($, Util, ModalDialog) {
 
             it('should have margin of 0px 20px 20px 20px', function() {
                 var $modalBody = $modal.find('.modal-body');
-                
+
                 //expect(Util.calculateDistance($modalBody, 'top')).toBe('20px'); 
                 expect(Util.calculateDistance($modalBody, 'right')).toBe('20px');
                 //expect(Util.calculateDistance($modalBody, 'bottom')).toBe('20px'); 
@@ -125,7 +156,7 @@ function runTest($, Util, ModalDialog) {
             });
         });
 
-        describe('Footer section', function() {  
+        describe('Footer section', function() {
             it('should have an Horizontal Rule of 1px #ebebee', function() {
                 var $modalFooter = $modal.find('.modal-footer');
 
