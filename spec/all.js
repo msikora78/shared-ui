cac26a9e816075d2cc0e57a0da0830eb189e93ff
vbarbin <git@vincentbarbin.net>
2013-03-15 17:58:50 -0400

    Modified unittests to include Important and Secondary Buttons

--- spec/all.js
+++ spec/all.js
@@ -2,7 +2,10 @@ define([
     'spec/tm/cookieSpec.js',
     'spec/tm/widgets/widgetFactory-spec',
     'spec/tm/widgets/primaryButton-spec',
+    'spec/tm/widgets/secondaryButton-spec',
+    'spec/tm/widgets/importantButton-spec',
     'spec/tm/widgets/modalDialog-spec'
     ], function($) {
 
 });
+
