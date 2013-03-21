c144ff1530a7046cb2155031db72ed8267e53af9
Jean-Francois Renaud <jeanfrancois.renaud@gmail.com>
2013-03-21 15:11:56 -0400

    Text input - Part III

--- demo/textInputFields-examples/all.js
+++ demo/textInputFields-examples/all.js
@@ -1,4 +1,4 @@
-define(['./simple-input', './error-input', './error-input-with-help'], function() {
+define(['./simple-input', './long-text-input', './error-input', './error-input-with-help'], function() {
     return {
         title: 'Text Input Fields',
         examples: Array.prototype.slice.call(arguments)
