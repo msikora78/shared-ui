9044b4b4ad222cdfdf5835cfaa35ca03acd315ca
vbarbin <git@vincentbarbin.net>
2013-04-02 14:40:37 -0400

    Added the "overview" section in the buttons examples.

--- demo/buttons-examples/all.js
+++ demo/buttons-examples/all.js
@@ -1,4 +1,4 @@
-define(['./primary-button', './primary-buttonWithLongText', './secondary-button', './secondary-buttonWithLongText', './important-button', './important-buttonWithLongText'], function() {
+define(['./overview', './primary-button', './primary-buttonWithLongText', './secondary-button', './secondary-buttonWithLongText', './important-button', './important-buttonWithLongText'], function() {
     return {
         title: 'Buttons',
         examples: Array.prototype.slice.call(arguments)
