55c2fece16a298bb15740b6dd5e1a9dced68cfb4
David Amador <davidamador@livenation.com>
2013-08-02 10:43:42 -0700

    updating text style demos

--- demo/textStyles-examples/gray-bg.js
+++ demo/textStyles-examples/gray-bg.js
@@ -4,9 +4,9 @@ define(['jquery'], function($) {
         legend: 'Dark Text on Gray Background',
         html:'\
 <div id="text-styles-container-gray">\n\
-  <h1>30px Museo Sans 500 &lt;h1&gt; <a href="javascript:;">with link style</a></h1>\n\
-  <h2>24px Museo Sans 500 &lt;h2&gt; <a href="javascript:;">with link style</a></h2>\n\
-  <p>14px Arial Regular &lt;p&gt; <a href="javascript:;">with link style</a></p>\n\
+  <h1>30px Museo Sans 500 &lt;h1&gt; <a href="#h1">with link style</a></h1>\n\
+  <h2>24px Museo Sans 500 &lt;h2&gt; <a href="#h2">with link style</a></h2>\n\
+  <p>14px Arial Regular &lt;p&gt; <a href="#p">with link style</a></p>\n\
 </div>'
     };
 
