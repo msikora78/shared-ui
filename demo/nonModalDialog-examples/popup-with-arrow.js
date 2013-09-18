e79ef1e44634973db01a6b17c52c6546f8f4ed52
vaudy <vincent.audy@gmail.com>
2013-09-18 10:18:44 -0400

    fixing popup test

--- demo/nonModalDialog-examples/popup-with-arrow.js
+++ demo/nonModalDialog-examples/popup-with-arrow.js
@@ -8,6 +8,11 @@ define(['jquery', 'widget!tm/widgets/popup'], function($) {
             <div class="demo-container row-fluid">\n\
                 <p class="span2"></p>\n\
                 <p class="span2">\n\
+                    <a class="popupWithArrow" \n\
+                    title="It\'s so simple to create a tooltip!"\n\
+                    data-placement="bottom-right"\n\
+                    data-show-arrow="true">Bottom-right</a></p>\n\
+                <p class="span2">\n\
                     <a class="popupWithArrow" style="margin-left: 30px" \n\
                         title="It\'s so simple to create a tooltip!"\n\
                         data-placement="top-right"\n\
@@ -22,16 +27,21 @@ define(['jquery', 'widget!tm/widgets/popup'], function($) {
                         title="It\'s so simple to create a tooltip!"\n\
                         data-placement="bottom-left"\n\
                         data-show-arrow="true">Bottom-left</a></p>\n\
-                <p class="span2">\n\
-                    <a class="popupWithArrow" \n\
-                    title="It\'s so simple to create a tooltip!"\n\
-                    data-placement="bottom-right"\n\
-                    data-show-arrow="true">Bottom-right</a></p>\n\
                 <p class="span2"></p>\n\
                 <div id="popupContainer" class="tm360"></div>\n\
             </div>\n\
             <div class="demo-container row-fluid">\n\
                 <p class="span2"></p>\n\
+                <p class="span2">\n\
+                    <a class="popupWithArrow" \n\
+                    title="It\'s so simple to create a tooltip!"\n\
+                    data-placement="right-bottom"\n\
+                    data-show-arrow="true">Right-bottom</a></p>\n\
+                <p class="span2">\n\
+                    <a class="popupWithArrow" style="margin-left: 30px" \n\
+                        title="It\'s so simple to create a tooltip!"\n\
+                        data-placement="right-top"\n\
+                        data-show-arrow="true">Right-top</a></p>\n\
                 <p class="span2">\n\
                     <a class="popupWithArrow" style="margin-left: 30px" \n\
                         title="It\'s so simple to create a tooltip!"\n\
@@ -42,16 +52,6 @@ define(['jquery', 'widget!tm/widgets/popup'], function($) {
                         title="It\'s so simple to create a tooltip!"\n\
                         data-placement="left-bottom"\n\
                         data-show-arrow="true">Left-bottom</a></p>\n\
-                <p class="span2">\n\
-                    <a class="popupWithArrow" style="margin-left: 30px" \n\
-                        title="It\'s so simple to create a tooltip!"\n\
-                        data-placement="right-top"\n\
-                        data-show-arrow="true">Right-top</a></p>\n\
-                <p class="span2">\n\
-                    <a class="popupWithArrow" \n\
-                    title="It\'s so simple to create a tooltip!"\n\
-                    data-placement="right-bottom"\n\
-                    data-show-arrow="true">Right-bottom</a></p>\n\
                 <p class="span2"></p>\n\
                 <div id="popupContainer" class="tm360"></div>\n\
             </div>',
